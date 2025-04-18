010e74b2238387c0866b5fa4a819a71716ad8e09
Nemanja Vukosav <nemanja.vukosav@gmail.com>
2025-04-18 13:52:02 +0200

    Added Post and Comment models, migrations, requests and controllers; Updated api routes;

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\Api\PostController;
+use App\Http\Controllers\CommentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('posts', PostController::class);
+Route::apiResource('posts.comments', CommentController::class)
+    ->scoped(['comment' => 'post']);
