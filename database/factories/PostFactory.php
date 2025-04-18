5037dea0c87157c73517988c88e6591ee89f1cd6
Nemanja Vukosav <nemanja.vukosav@gmail.com>
2025-04-18 16:11:49 +0200

    Added Post and Comment factories and seeders

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence(),
+            'content' => $this->faker->paragraph(),
+            'created_at' => fake()->dateTimeBetween('-3 year'),
+        ];
+    }
+}
